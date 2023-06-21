c638b24881407350c7d80adbf45c531c4cfcd7ca
nmartineli <nataliamartineli@gmail.com>
2023-06-21 18:49:59 -0300

    Setup routes with react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { ThemeProvider } from 'styled-components';
+import { BrowserRouter } from 'react-router-dom';
 import { defaultTheme } from './styles/themes/default';
+import { Router } from './Router';
+import { GlobalStyle } from './styles/globals';
 
 export function App() {
 	return (
 		<ThemeProvider theme={defaultTheme}>
-			<>
-				<h1>Hello</h1>
-			</>
+			<BrowserRouter>
+				<Router />
+			</BrowserRouter>
+			<GlobalStyle />
 		</ThemeProvider>
 	);
 }
