65b264ff15b37a027551e3eca711721a898107c1
nmartineli <nataliamartineli@gmail.com>
2023-07-04 20:21:29 -0300

    Moved handle cart update function into cart context

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -1,17 +1,24 @@
 import { createContext, ReactNode, useReducer } from 'react';
 import { CartProduct, cartReducer } from '../reducers/cart/reducers';
 import { addToCart, updateCart } from '../reducers/cart/actions';
+import { IDrink } from '../hooks/useFetchProducts';
 
 interface CartContextType {
 	cart: CartProduct[];
 	addProductToCart: (cartProduct: CartProduct) => void;
 	updateProductQuantity: (cartProduct: CartProduct, productCartIndex: number) => void;
+	handleAddToCart: (event: React.FormEvent<HTMLFormElement>, productAddedToCart: CartProductProps) => void;
 }
 
 export const CartContext = createContext({} as CartContextType);
 
 interface CartContextProviderProps {
 	children: ReactNode;
+}
+
+export interface CartProductProps extends IDrink {
+	quantity: number;
+	totalPrice: number;
 }
 
 export function CartContextProvider({ children }: CartContextProviderProps) {
@@ -29,12 +36,27 @@ export function CartContextProvider({ children }: CartContextProviderProps) {
 		dispatch(updateCart(productAddedToCart, productCartIndex));
 	}
 
+	const findProductIndexInCart = (productBeingAdded: CartProductProps) => {
+		return cart.findIndex((product) => product.id === productBeingAdded.id);
+	};
+
+	const handleAddToCart = (event: React.FormEvent<HTMLFormElement>, productAddedToCart: CartProductProps) => {
+		event.preventDefault();
+		const productCartIndex = findProductIndexInCart(productAddedToCart);
+		if (productCartIndex >= 0) {
+			updateProductQuantity(productAddedToCart, productCartIndex);
+			return;
+		}
+		addProductToCart(productAddedToCart);
+	};
+
 	return (
 		<CartContext.Provider
 			value={{
 				cart,
 				addProductToCart,
 				updateProductQuantity,
+				handleAddToCart,
 			}}
 		>
 			{children}
